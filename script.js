4c408b950b3ffac06e6cefeecfb6f169137d4db0
Benedicta <benedictauche.onyebuchi@gmail.com>
2023-04-22 23:25:42 +0100

    added media queries to make webapp responsive

--- script.js
+++ script.js
@@ -16,18 +16,20 @@ let inputGroup = document.querySelector('.input-group');
 let todoItem = document.querySelector('.todo-item');
 let todoBottom = document.querySelector('.todo-bottom');
 let addBtn = document.querySelector('.addbtn');
-// let input = document.getElementById('#new-todo');
 let checkbox = document.querySelector('.check-box');
 let todoForm = document.querySelector('form');
 let todoInput = document.getElementById('new-todo');
 let todoList = document.querySelector('.todo-items');
-// let todoItems = document.querySelector('.todo-items');
+let attribution = document.querySelector('.attribution');
 let deleteBtn = document.querySelector('deletebtn');
-let priority = document.querySelector('priority')
+let priority = document.querySelector('#priority')
 let itemCheckbox = document.querySelector('.checkbox');
 let remain = document.querySelector('.remain');
-let filters = document.querySelector('filters');
-let completed = document.querySelector('complete');
+let filters = document.querySelector('.filters');
+let all = document.querySelector('.all');
+let active = document.querySelector('.activebtn');
+let completed = document.querySelector('li .complete');
+let clear = document.querySelector('.clear');
 
 
 themeBtn.addEventListener('click', function () {
@@ -36,6 +38,7 @@ themeBtn.addEventListener('click', function () {
         themeBtn.innerHTML = '<img src="images/icon-moon.svg" alt="Toggle theme">';
         body.style.backgroundColor = 'var(--light-grayish-blue)';
         inputGroup.style.backgroundColor = 'var(--light-grayish-blue)';
+        // todoList.style.backgroundColor = 'var(--light-grayish-blue)';
         todoItem.style.backgroundColor = 'var(--light-grayish-blue)';
         todoItem.style.borderBottom = 'none';
         todoBottom.style.backgroundColor = 'var(--light-grayish-blue)';
@@ -44,6 +47,7 @@ themeBtn.addEventListener('click', function () {
         themeBtn.innerHTML = '<img src="images/icon-sun.svg" alt="Toggle theme">';
         body.style.backgroundColor = 'var(--dark-blue)';
         inputGroup.style.backgroundColor = 'var(--darker-desaturated-blue)';
+        // todoList.style.backgroundColor = 'var(--darker-desaturated-blue)';
         todoItem.style.backgroundColor = 'var(--darker-desaturated-blue)';
         todoItem.style.borderBottom = '1px solid var(--dark-grayish-blue-dark)';
         todoBottom.style.backgroundColor = 'var(--darker-desaturated-blue)';
@@ -74,7 +78,7 @@ function addTodoToLibrary(e) {
 
 todoForm.addEventListener('submit', addTodoToLibrary);
 let todoCount = 0;
-
+let allCount = 0;
 
 
 function addItems() {
@@ -87,6 +91,7 @@ function addItems() {
 
             let newTodoItem = document.createElement('li');
             newTodoItem.classList.add('todo-item');
+            newTodoItem.setAttribute('draggable', 'true')
 
             let itemCheckbox = document.createElement('div');
             itemCheckbox.classList.add('item-checkbox');
@@ -99,11 +104,20 @@ function addItems() {
                 itemText.innerHTML = '<p>' + inputValue + '</p>';
                 newTodoItem.appendChild(itemText);
                 newTodoItem.style.textDecoration = 'line-through';
+                itemCheckbox.innerHTML = '<input type="checkbox" class="checkbox" checked><label></label>';
+                if (newTodoItem.style.textDecoration = 'line-through') {
+                    itemCheckbox.addEventListener('click', () => {
+                        newTodoItem.style.textDecoration = 'none';
+
+                    })
+                } else {
+                    newTodoItem.style.textDecoration = 'line-through';
+                }
+
             } else {
                 itemText.innerHTML = '<p>' + inputValue + '</p>';
                 newTodoItem.appendChild(itemText);
             }
-
 
 
             let itemDelete = document.createElement('div');
@@ -122,10 +136,9 @@ function addItems() {
 
             todoList.appendChild(newTodoItem);
 
-
-            // if (itemCheckbox.checked) {
-
-            // }
+            // itemCheckbox.addEventListener('click', () => {
+            //     newTodoItem.style.textDecoration = 'line-through';
+            // })
 
             console.log(todos)
 
@@ -133,14 +146,154 @@ function addItems() {
         }
     }
 };
+completed.addEventListener('click', completedTodos)
+active.addEventListener('click', activeTodos)
+all.addEventListener('click', allTodos)
+clear.addEventListener('click', () => {
+    const completeTodos = todos.filter(function (todo) {
+        return (todo.check === true)
+    })
+    completeTodos.forEach(function (todo) {
+        completeTodos.splice(0, completeTodos.length)
+    })
+    todoList.innerHTML = ''
+    console.log(completeTodos)
+
+})
 
 
-// let completed = document.querySelector('complete');
 
-// completed.addEventListener('click', function () {
-//     todos.forEach(completedTodos)
-// })
+function completedTodos() {
+    const completeTodos = todos.filter(function (todo) {
+        return (todo.check === true)
 
-// function completedTodos() {
-//     console.log(newTodoItem)
+    })
+    todoList.innerHTML = ''
+    completeTodos.forEach(function (todo) {
+        const newTodoItem = document.createElement('li');
+        newTodoItem.classList.add('todo-item');
+        let itemCheckbox = document.createElement('div');
+        itemCheckbox.classList.add('item-checkbox');
+        itemCheckbox.innerHTML = '<input type="checkbox" class="checkbox" checked><label></label>';
+        newTodoItem.appendChild(itemCheckbox);
+        let itemText = document.createElement('div');
+        itemText.classList.add('item-text');
+        itemText.innerHTML = `<p>${todo.todoValue}</p>`;
+        newTodoItem.appendChild(itemText);
+        newTodoItem.style.textDecoration = 'line-through';
+        let itemDelete = document.createElement('div');
+        itemDelete.classList.add('item-delete');
+        itemDelete.innerHTML = '<button aria-label="Delete todo" class="deletebtn"><img src="./images/icon-cross.svg" alt="Delete todo"></button>';
+        newTodoItem.appendChild(itemDelete);
+
+        itemDelete.addEventListener('click', function () {
+            todos.splice(i, 1);
+            todoList.removeChild(newTodoItem);
+            remaining = todos.length;
+            remain.innerHTML = remaining;
+        })
+        let remaining = todos.length;
+        remain.innerHTML = remaining;
+
+        todoList.appendChild(newTodoItem);
+
+    });
+    console.log(completeTodos)
+}
+
+
+function activeTodos() {
+    const actTodos = todos.filter(function (todo) {
+        return (todo.check === false)
+    })
+    todoList.innerHTML = '';
+    actTodos.forEach(function (todo) {
+        const newTodoItem = document.createElement('li');
+        newTodoItem.classList.add('todo-item');
+        let itemCheckbox = document.createElement('div');
+        itemCheckbox.classList.add('item-checkbox');
+        itemCheckbox.innerHTML = '<input type="checkbox" class="checkbox"><label></label>';
+        newTodoItem.appendChild(itemCheckbox);
+        let itemText = document.createElement('div');
+        itemText.classList.add('item-text');
+        itemText.innerHTML = `<p>${todo.todoValue}</p>`;
+        newTodoItem.appendChild(itemText);
+        newTodoItem.style.textDecoration = 'none';
+        let itemDelete = document.createElement('div');
+        itemDelete.classList.add('item-delete');
+        itemDelete.innerHTML = '<button aria-label="Delete todo" class="deletebtn"><img src="./images/icon-cross.svg" alt="Delete todo"></button>';
+        newTodoItem.appendChild(itemDelete);
+
+        itemDelete.addEventListener('click', function () {
+            todos.splice(i, 1);
+            todoList.removeChild(newTodoItem);
+            remaining = todos.length;
+            remain.innerHTML = remaining;
+        })
+        let remaining = todos.length;
+        remain.innerHTML = remaining;
+
+        todoList.appendChild(newTodoItem);
+    })
+    console.log(actTodos)
+}
+
+
+function allTodos() {
+    const allOfTodos = todos.filter(function (todo) {
+        return (todo.check === false || todo.check === true)
+    })
+    todoList.innerHTML = '';
+    for (let i = todoCount; i < todos.length; i++) {
+        allOfTodos.forEach(function (todo) {
+            const newTodoItem = document.createElement('li');
+            newTodoItem.classList.add('todo-item');
+            let itemCheckbox = document.createElement('div');
+            itemCheckbox.classList.add('item-checkbox');
+
+            if (todo.check == true) {
+                itemCheckbox.innerHTML = '<input type="checkbox" class="checkbox" checked><label></label>';
+            }
+            else {
+                itemCheckbox.innerHTML = '<input type="checkbox" class="checkbox"><label></label>';
+            }
+            newTodoItem.appendChild(itemCheckbox);
+            let itemText = document.createElement('div');
+            itemText.classList.add('item-text');
+            itemText.innerHTML = `<p>${todo.todoValue}</p>`;
+            newTodoItem.appendChild(itemText);
+            if (todo.check == true) {
+                newTodoItem.style.textDecoration = 'line-through';
+            }
+            else {
+                newTodoItem.style.textDecoration = 'none';
+            }
+
+            let itemDelete = document.createElement('div');
+            itemDelete.classList.add('item-delete');
+            itemDelete.innerHTML = '<button aria-label="Delete todo" class="deletebtn"><img src="./images/icon-cross.svg" alt="Delete todo"></button>';
+            newTodoItem.appendChild(itemDelete);
+
+            itemDelete.addEventListener('click', function () {
+                todos.splice(i, 1);
+                todoList.removeChild(newTodoItem);
+                remaining = todos.length;
+                remain.innerHTML = remaining;
+            })
+            let remaining = todos.length;
+            remain.innerHTML = remaining;
+
+            todoList.appendChild(newTodoItem);
+        })
+    }
+    console.log(allOfTodos)
+}
+
+
+
+// function allTodos() {
+//     const allOfTodos = todos.filter(function (todo) {
+//         return (todo.check === false || todo.check === true)
+//     })
+//     console.log(allOfTodos)
 // }
